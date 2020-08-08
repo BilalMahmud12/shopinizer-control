01ce0673dc6e1a78525016d7f12023d0bfe64585
Rakan Awajan <rkawajan@gmail.com>
2020-08-08 04:27:41 +0300

    create single page, fix some styles

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -1,8 +1,31 @@
-var app = new Vue({
+Vue.component('treeselect', VueTreeselect.Treeselect)
+
+new Vue({
     el: '#app',
     data: {
+        // treeselect dump
+        treeselect_value: null,
+        treeselect_options: [ {
+            id: 'a',
+            label: 'a',
+            children: [ {
+            id: 'aa',
+            label: 'aa',
+            }, {
+            id: 'ab',
+            label: 'ab',
+            } ],
+        }, {
+            id: 'b',
+            label: 'b',
+        }, {
+            id: 'c',
+            label: 'c',
+        } ],
     },
 });
+
+Dropzone.autoDiscover = false;
 
 $(document).ready(function() {
     tippy('[data-tippy-content]', {
@@ -10,5 +33,15 @@ $(document).ready(function() {
     });
     $('#records-table').DataTable({
         responsive: true,
+    });
+    new Quill('#editor', {
+        theme: 'snow'
+    });
+
+    $("#upload-widget").dropzone({
+        url: "/upload",
+        dictDefaultMessage: "Drag & Drop images here to upload.",
+        uploadMultiple: true,
+        capture: true,
     });
  });
