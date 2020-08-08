d8a2b6354ceb4a76b668331037e4a1f47a4359c2
Rakan Awajan <rkawajan@gmail.com>
2020-08-08 06:24:43 +0300

    make datatable selectable

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -28,16 +28,61 @@ new Vue({
 Dropzone.autoDiscover = false;
 
 $(document).ready(function() {
+    // Tippy
     tippy('[data-tippy-content]', {
         placement: 'right'
     });
-    $('#records-table').DataTable({
+
+    // Records tables
+    var mainTable = $('#records-table').DataTable({
+        columnDefs: [
+            {
+                "targets": 'no-sort',
+                "orderable": false,
+            },
+            {
+                targets: 0,
+                data: "select",
+                searchable: false,
+                orderable: false,
+                className: 'select-checkbox',
+                width: "4%"
+            }
+        ],
         responsive: true,
+        select: {
+            style: 'multi',
+            selector: 'td:first-child'
+        },
+        order: [[ 1, 'asc' ]]
     });
+
+    $('.select-checkbox').on('click', function() {
+        $(this).find('.select-row').prop("checked", !$(this).find('.select-row').prop("checked"));
+    });
+
+    $('.select-all').click(function() {
+        var all = mainTable.rows({ search: 'applied' }).count();
+        var selectedRows = mainTable.rows({ selected: true, search: 'applied' }).count();
+
+        if (selectedRows < all) {
+            mainTable.rows({ search: 'applied' }).deselect();
+            mainTable.rows({ search: 'applied' }).select();
+            $(this).find('input').prop("checked", true);
+            $('#records-table').find('.select-row').prop("checked", true);
+        } else {
+            mainTable.rows({ search: 'applied' }).deselect();
+            $(this).find('input').prop("checked", false);
+            $('#records-table').find('.select-row').prop("checked", false);
+        }
+    });
+
+    // Quill editor
     new Quill('#editor', {
         theme: 'snow'
     });
 
+    // Upload images
     $("#upload-widget").dropzone({
         url: "/upload",
         dictDefaultMessage: "Drag & Drop images here to upload.",
