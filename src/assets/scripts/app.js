d632e1e08aa0db51d49863086f4355781f1b7a9f
Rakan Awajan <rkawajan@gmail.com>
2020-08-10 20:56:01 +0300

    complete product fields

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -5,6 +5,8 @@ new Vue({
     data: {
         inventory: false,
         inventory_level: 1,
+        purchasability: 1,
+        related_products: true,
 
         // treeselect dump
         treeselect_value: null,
@@ -26,86 +28,89 @@ new Vue({
             label: 'c',
         } ],
     },
-});
+    components: {
+        vuejsDatepicker
+    },
+    mounted: function() {
 
-Dropzone.autoDiscover = false;
+        // Tippy
+        tippy('[data-tippy-content]', {
+            placement: 'right'
+        });
 
-$(document).ready(function() {
-    // Tippy
-    tippy('[data-tippy-content]', {
-        placement: 'right'
-    });
-
-    // Records tables
-    var mainTable = $('#records-table').DataTable({
-        columnDefs: [
-            {
-                "targets": 'no-sort',
-                "orderable": false,
+        // Records tables
+        var mainTable = $('#records-table').DataTable({
+            columnDefs: [
+                {
+                    "targets": 'no-sort',
+                    "orderable": false,
+                },
+                {
+                    targets: 0,
+                    data: "select",
+                    searchable: false,
+                    orderable: false,
+                    className: 'select-checkbox',
+                    width: "4%"
+                }
+            ],
+            responsive: true,
+            select: {
+                style: 'multi',
+                selector: 'td:first-child'
             },
-            {
-                targets: 0,
-                data: "select",
-                searchable: false,
-                orderable: false,
-                className: 'select-checkbox',
-                width: "4%"
+            order: [[ 1, 'asc' ]]
+        });
+
+        $('.select-checkbox').on('click', function() {
+            $(this).find('.select-row').prop("checked", !$(this).find('.select-row').prop("checked"));
+        });
+
+        $('.select-all').click(function() {
+            var all = mainTable.rows({ search: 'applied' }).count();
+            var selectedRows = mainTable.rows({ selected: true, search: 'applied' }).count();
+
+            if (selectedRows < all) {
+                mainTable.rows({ search: 'applied' }).deselect();
+                mainTable.rows({ search: 'applied' }).select();
+                $(this).find('input').prop("checked", true);
+                $('#records-table').find('.select-row').prop("checked", true);
+            } else {
+                mainTable.rows({ search: 'applied' }).deselect();
+                $(this).find('input').prop("checked", false);
+                $('#records-table').find('.select-row').prop("checked", false);
             }
-        ],
-        responsive: true,
-        select: {
-            style: 'multi',
-            selector: 'td:first-child'
-        },
-        order: [[ 1, 'asc' ]]
-    });
+        });
 
-    $('.select-checkbox').on('click', function() {
-        $(this).find('.select-row').prop("checked", !$(this).find('.select-row').prop("checked"));
-    });
+        // Quill editor init
+        new Quill('#editor', {
+            theme: 'snow'
+        });
 
-    $('.select-all').click(function() {
-        var all = mainTable.rows({ search: 'applied' }).count();
-        var selectedRows = mainTable.rows({ selected: true, search: 'applied' }).count();
+        // Select2 init
+        $(".select2-df").select2({});
 
-        if (selectedRows < all) {
-            mainTable.rows({ search: 'applied' }).deselect();
-            mainTable.rows({ search: 'applied' }).select();
-            $(this).find('input').prop("checked", true);
-            $('#records-table').find('.select-row').prop("checked", true);
-        } else {
-            mainTable.rows({ search: 'applied' }).deselect();
-            $(this).find('input').prop("checked", false);
-            $('#records-table').find('.select-row').prop("checked", false);
-        }
-    });
+        Dropzone.autoDiscover = false;
 
-    // Quill editor
-    new Quill('#editor', {
-        theme: 'snow'
-    });
+        // Upload images init
+        $("#upload-widget").dropzone({
+            url: "/upload",
+            dictDefaultMessage: "Drag & Drop images here to upload.",
+            uploadMultiple: true,
+            capture: true,
+        });
 
-    // Select2
-    $(".select2-df").select2({});
+        // Variations repeater
+        $('.variations-repeater').repeater({
+            repeaters: [{
+                selector: '.variation-values-repeater'
+            }]
+        });
 
-    // Upload images
-    $("#upload-widget").dropzone({
-        url: "/upload",
-        dictDefaultMessage: "Drag & Drop images here to upload.",
-        uploadMultiple: true,
-        capture: true,
-    });
+        // Customizations repeater
+        $('.customizations-repeater').repeater({});
 
-    // Variations repeater
-    $('.variations-repeater').repeater({
-        repeaters: [{
-            selector: '.variation-values-repeater'
-        }]
-    });
-
-    // Customizations repeater
-    $('.customizations-repeater').repeater({});
-
-    // Custom Fields repeater
-    $('.custom-fields-repeater').repeater({});
+        // Custom Fields repeater
+        $('.custom-fields-repeater').repeater({});
+    }
 });
